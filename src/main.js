ff88be40fd6f7d04c9e00baf6ba266989b4e139e
michaelmehr <michaelmehr0@gmail.com>
2023-06-02 19:26:47 -0500

    created playRound() in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,51 @@
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice > 1) {
-        return 'Rock';
+        return 'rock';
     } else if (computerChoice > 0) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let pChoice = playerSelection.toLowerCase();
+    if (pChoice === 'rock') {
+        if (computerSelection === 'rock') {
+            return "You Tied! Both chose Rock";
+        }
+        if (computerSelection === 'paper') {
+            return "You Lose! Paper beats Rock";
+        }
+        if (computerSelection === 'scissors') {
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    if (pChoice === 'paper') {
+        if (computerSelection === 'rock') {
+            return "You Win! Paper beats Rock";
+        }
+        if (computerSelection === 'paper') {
+            return "You Tied! Both chose Paper";
+        }
+        if (computerSelection === 'scissors') {
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+    if (pChoice === 'scissors') {
+        if (computerSelection === 'rock') {
+            return "You Lose! Rock beats Scissors";
+        }
+        if (computerSelection === 'paper') {
+            return "You Win! Scissors beats Paper";
+        }
+        if (computerSelection === 'scissors') {
+            return "You Tied! Both chose Scissors";
+        }
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
