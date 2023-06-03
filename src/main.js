49311cad3025fc5c00e70cd05b94e6def3967842
michaelmehr <michaelmehr0@gmail.com>
2023-06-02 19:44:28 -0500

    created game() function. made adjustments to playRound() so the scores are updated at the end of each round.

--- src/main.js
+++ src/main.js
@@ -16,28 +16,34 @@ function playRound(playerSelection, computerSelection) {
             return "You Tied! Both chose Rock";
         }
         if (computerSelection === 'paper') {
+            computerScore++;
             return "You Lose! Paper beats Rock";
         }
         if (computerSelection === 'scissors') {
+            playerScore++;
             return "You Win! Rock beats Scissors";
         }
     }
     if (pChoice === 'paper') {
         if (computerSelection === 'rock') {
+            playerScore++;
             return "You Win! Paper beats Rock";
         }
         if (computerSelection === 'paper') {
             return "You Tied! Both chose Paper";
         }
         if (computerSelection === 'scissors') {
+            computerScore++;
             return "You Lose! Scissors beats Paper";
         }
     }
     if (pChoice === 'scissors') {
         if (computerSelection === 'rock') {
+            computerScore++;
             return "You Lose! Rock beats Scissors";
         }
         if (computerSelection === 'paper') {
+            playerScore++;
             return "You Win! Scissors beats Paper";
         }
         if (computerSelection === 'scissors') {
@@ -46,6 +52,26 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    playerScore = 0;
+    computerScore = 0;
+    let playerSelection, computerSelection;
+    const LENGTH_OF_GAME = 5
+
+    for (let i = 0; i < LENGTH_OF_GAME; i++) {
+        playerSelection = prompt("Rock, paper, or scissors?");
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player Score: ${playerScore}. Computer Score: ${computerScore}.`)
+    }
+    if (playerScore === computerScore) {
+        console.log("You tied the game!");
+    } else if (playerScore > computerScore) {
+        console.log("You win the game!");
+    } else {
+        console.log("You lose the game!");
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
