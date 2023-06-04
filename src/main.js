279e77f6e74d95af041c88e891b512043368bc9b
michaelmehr <michaelmehr0@gmail.com>
2023-06-03 23:16:36 -0500

    adjusted playRound() to play game as intended

--- src/main.js
+++ src/main.js
@@ -13,43 +13,35 @@ function playRound(playerSelection, computerSelection) {
     let pChoice = playerSelection.toLowerCase();
     if (pChoice === 'rock') {
         if (computerSelection === 'rock') {
-            return "tie";
         }
         if (computerSelection === 'paper') {
             computerScore++;
-            return "lose";
         }
         if (computerSelection === 'scissors') {
             playerScore++;
-            return "win";
         }
     }
     if (pChoice === 'paper') {
         if (computerSelection === 'rock') {
             playerScore++;
-            return "win";
         }
         if (computerSelection === 'paper') {
-            return "tie";
         }
         if (computerSelection === 'scissors') {
             computerScore++;
-            return "lose";
         }
     }
     if (pChoice === 'scissors') {
         if (computerSelection === 'rock') {
             computerScore++;
-            return "lose";
         }
         if (computerSelection === 'paper') {
             playerScore++;
-            return "win";
         }
         if (computerSelection === 'scissors') {
-            return "tie";
         }
     }
+    updateScores();
 }
 
 function updateScores() {
@@ -73,6 +65,6 @@ const winnerDisplay = document.querySelector('#winner');
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playRound(button.id, getComputerChoice()));
+        playRound(button.id, getComputerChoice());
     })
 })
